9fd84b8a7fe79fc77c7f69bfa9bea8c47e9be452
jkintscher <joschka@kintscher.me>
2022-08-16 14:47:35 +0200

    Add basic subscription logic to service worker

--- service-worker.js
+++ service-worker.js
@@ -10,3 +10,36 @@ importScripts('service-worker-utils.js')
 // If you want to import a file that is deeper in the file hierarchy of your
 // extension, simply do `importScripts('path/to/file.js')`.
 // The path should be relative to the file `manifest.json`.
+
+async function initiateSubscription() {
+  const subscription = await self.registration.pushManager.getSubscription()
+
+  if (subscription) {
+    console.log('Already subscribed!', subscription.endpoint)
+    return
+  }
+
+  subscribe()
+}
+
+async function subscribe() {
+  const vapidPublicKey = 'BA6FsKnQSkEaHyww8ZJArtca-30SYPlq7034KJUc3PbHVyCQ9d8hzdwCkIgWQ6wC8uuEvm1IJm7hrht_qAAzLpc'
+  const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey)
+
+  const subscription = await self.registration.pushManager.subscribe({
+    applicationServerKey: convertedVapidKey,
+    userVisibleOnly: true,
+  });
+
+  console.log('Subscribed at', subscription.endpoint);
+
+  fetch('http://localhost:3003/register', {
+    // TODO: Include current account/user here, and access token to authorize
+    body: JSON.stringify({ subscription }),
+    headers: { 'Content-type': 'application/json' },
+    method: 'post',
+  })
+    .then((response) => {
+      console.log('Status: ', response.status)
+    })
+}

e0f27aa8add8430098dee5e5a9b84c01058d044d
jkintscher <joschka@kintscher.me>
2022-08-16 14:42:58 +0200

    Add and configure web-push

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const webPush = require('web-push')
+
+// Configure web-push
+
+const keys = {
+  privateKey: 'lLettub3VDaY0ci0XgbSHD6lCa0f0N8y7VAwSrEEO34',
+  publicKey: 'BA6FsKnQSkEaHyww8ZJArtca-30SYPlq7034KJUc3PbHVyCQ9d8hzdwCkIgWQ6wC8uuEvm1IJm7hrht_qAAzLpc',
+  subject: 'mailto: <joschka@getharvest.com>',
+}
+
+webPush.setVapidDetails(
+  keys.subject,
+  keys.publicKey,
+  keys.privateKey
+)
 
 // Configure middleware and routes
 

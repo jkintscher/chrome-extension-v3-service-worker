67c357c5ecb2e475c12b0e03e0542bf42be210f5
jkintscher <joschka@kintscher.me>
2022-08-16 20:04:08 +0200

    Send message to iframe’s parent

--- popup/iframe.js
+++ popup/iframe.js
@@ -1,2 +1,7 @@
 console.log('iframe window.opener', window.opener)
 console.log('iframe window.parent', window.parent)
+
+window.parent.postMessage(
+  'here’s a message for my dad',
+  'chrome-extension://ofniiffbnieifjdgaeniekjkmnfgnmbb'
+)
